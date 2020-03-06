f5c9e08626fc688ff85a0662b6b2013749a32d8c
Jack Whitter-Jones <40804987+jwhitt3r@users.noreply.github.com>
2020-03-06 17:03:02 +0000

    Add files via upload

--- src/Copperhead85.py
+++ src/Copperhead85.py
@@ -2,9 +2,9 @@ import argparse
 import base64
 
 parser = argparse.ArgumentParser(description='Welcome to the CopperHead85 USW CTF 2020 Challenge!', epilog='The UPX CyberChef is the best in packing!')
-parser.add_argument('-p', action='store', type=str, dest="input_password", help="the password used to compare to answer", default=True)
+parser.add_argument('-p', action='store', type=str, dest="input_password", help="the password field used to compare to answer. Remember to try quotes around the flag if the program/terminal doesn't allow you to enter the flag e.g., 'Flag' or \"Flag\"", default=True)
 
-zxcvf = "6Z6p_ATDHq@:V>T<DusS0JY=L<(6"
+zxcvf = "<DutG6Z6p_ATDHq@:WA:=#)Hb0LJf/I/"
 args = parser.parse_args()
 
 user_input = args.input_password
